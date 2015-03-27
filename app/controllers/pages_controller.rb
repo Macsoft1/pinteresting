91cd6ad5527f50d86f0b85dcaf5e9037408cd80f
RJ McNair <rmcnair2012@yahoo.com>
2015-03-26 23:44:38 -0400

    Strap variables cutomize

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def services
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/services'
+
+  get 'pages/contact'
+
   root "pages#home"
   get "about" => "pages#about"
+  get "services" => "pages#services"
+  get "contact" => "pages#contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
